4199a405206797195dd97fdfd81912b8e1b252d8
XuLiang <liangxu_wd@163.com>
2019-11-22 16:29:45 +0800

    the second file

---
+++ t2.f90
@@ -0,0 +1,20 @@
+program main
+      implicit none
+
+      type :: person_type
+              character(len = 12) :: name = ''
+              character(len = 2) :: sex
+              integer :: age
+      endtype person_type
+
+      type(person_type) :: personl, person_temp
+
+
+      personl = person_type("小花", "女", 23)
+      write(*,*) "persionl:",personl
+
+      person_temp = person_type("小刘", "男", 22)
+      personl = person_temp
+      write(*,*) "personl:",personl
+
+endprogram main
